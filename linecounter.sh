4e7e1b08d7387c474e9110f8a936e991f982a3d3
Samuel-Clarke123 <clarkepearl44@gmail.com>
2018-11-15 10:52:13 +1100

    linecounter hotfix

--- linecounter.sh
+++ linecounter.sh
@@ -10,13 +10,13 @@ a=$(find "$BASEDIR" -not \( -name node_modules -prune -o -name lib -prune -o -na
 
 printf "\nTotal (with package-lock):\n"
 
-a=$(find "$BASEDIR" -not \( -name node_modules -prune -o -name lib  \) -iregex '.*\.\(js\|json\|ejs\|html\|php\|sql\|css\|c\|cpp\|cs\|env\|sh\)' -exec sh -c 'cat "$1" | wc -l' - {} \;);b="$(echo "$a" | tr " " "\n")";readarray -t c <<<"$b";d=0;for i in "${c[@]}";do d=$((i+d));done;echo "$d"
+a=$(find "$BASEDIR" -not \( -name node_modules -prune -o -name lib -prune  \) -iregex '.*\.\(js\|json\|ejs\|html\|php\|sql\|css\|c\|cpp\|cs\|env\|sh\)' -exec sh -c 'cat "$1" | wc -l' - {} \;);b="$(echo "$a" | tr " " "\n")";readarray -t c <<<"$b";d=0;for i in "${c[@]}";do d=$((i+d));done;echo "$d"
 
 printf "\nTotal (with package-lock and frontend libs):\n"
 
-a=$(find "$BASEDIR" -not \( -name node_modules \) -iregex '.*\.\(js\|json\|ejs\|html\|php\|sql\|css\|c\|cpp\|cs\|env\|sh\)' -exec sh -c 'cat "$1" | wc -l' - {} \;);b="$(echo "$a" | tr " " "\n")";readarray -t c <<<"$b";d=0;for i in "${c[@]}";do d=$((i+d));done;echo "$d"
+a=$(find "$BASEDIR" -not \( -name node_modules -prune \) -iregex '.*\.\(js\|json\|ejs\|html\|php\|sql\|css\|c\|cpp\|cs\|env\|sh\)' -exec sh -c 'cat "$1" | wc -l' - {} \;);b="$(echo "$a" | tr " " "\n")";readarray -t c <<<"$b";d=0;for i in "${c[@]}";do d=$((i+d));done;echo "$d"
 
-printf "\nTotal (with package-lock and node modules):\n"
+printf "\nTotal (with package-lock and node modules and frontend libs):\n"
 
 a=$(find "$BASEDIR" -iregex '.*\.\(js\|json\|ejs\|html\|php\|sql\|css\|c\|cpp\|cs\|env\|sh\)' -exec sh -c 'cat "$1" | wc -l' - {} \;);b="$(echo "$a" | tr " " "\n")";readarray -t c <<<"$b";d=0;for i in "${c[@]}";do d=$((i+d));done;echo "$d"
 
